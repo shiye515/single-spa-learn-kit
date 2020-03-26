3bd28a6a7c3f0a8b225b55410f3310297f979d7b
jiashuo <jiashuo@didichuxing.com>
2020-03-26 14:19:21 +0800

    with dva

--- app1/app.js
+++ app1/app.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { HashRouter, Link } from 'react-router-dom';
-function Root() {
+import React from "react";
+import { HashRouter, Link } from "react-router-dom";
+import dva, { connect } from "dva";
+
+const app = dva();
+app.model({
+  namespace: "app1",
+  state: 0,
+  reducers: {
+    add(count) {
+      return count + 1;
+    },
+    minus(count) {
+      return count - 1;
+    }
+  }
+});
+
+const App = connect(props => props)(function(props) {
   return (
     <HashRouter>
-      123
-      <Link to='/app2'>app2 link</Link>
-      <Link to='/app3'>app3 link</Link>
+      <h2>
+        {props.app1}
+        <button
+          key="add"
+          onClick={() => {
+            props.dispatch({ type: "app1/add" });
+          }}
+        >
+          +
+        </button>
+        <button
+          key="minus"
+          onClick={() => {
+            props.dispatch({ type: "app1/minus" });
+          }}
+        >
+          -
+        </button>
+      </h2>
+
+      <Link to="/app2">app2 link</Link>
+      <Link to="/app3">app3 link</Link>
     </HashRouter>
   );
-}
-export default Root;
+});
+
+app.router(() => <App />);
+export default app.start();
